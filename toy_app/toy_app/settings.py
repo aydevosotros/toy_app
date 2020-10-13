75c6affd1f15f743ccfadd1613cd91d9d9a6129b
Antonio Molina <antonio.molina@qustodio.com>
2020-10-13 15:42:49 +0200

    Monitored db and added some more match expressions

--- toy_app/toy_app/settings.py
+++ toy_app/toy_app/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'toy_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django_prometheus.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
