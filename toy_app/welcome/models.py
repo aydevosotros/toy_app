b149e02f9f05551b670aa39b2ac06039b1321226
Antonio Molina <antonio.molina@qustodio.com>
2020-11-02 11:00:57 +0100

    Added publish doc image on registry

--- toy_app/welcome/models.py
+++ toy_app/welcome/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django_prometheus.models import ExportModelOperationsMixin
 
 
-# Create your models here.
 class Visit(ExportModelOperationsMixin('visit'), models.Model):
     ip = models.GenericIPAddressField(unique=True)
     last_visit = models.DateTimeField()
